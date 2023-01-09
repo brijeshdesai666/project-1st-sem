268c20d535f2b1b28a38c12fef3f9957ec566a5c
brijesh desai <110248235+brijeshdesai666@users.noreply.github.com>
2023-01-09 17:06:57 +0530

    Add files via upload

--- electricity_bill.c
+++ electricity_bill.c
@@ -1,43 +1,45 @@
-/*
-  village area = per unit 6.5  unit > 24 7.8
-  city area = per unit
-  industril area = per unit 8 to 10
-  agriculture line =  per unit 0.60 same for all  unit
-*/
+// Electricity Bil Calulator using c
+
 #include <stdio.h>
+#include<conio.h>
+
+// Make a function for print Electricity bill
 void printBill(float total_used, int fix_Charge)
 {
   float totalAmountToPay = total_used + fix_Charge;
 
   printf("Your Total amount to pay is ---->  %.2f   with applied fix charge %d\n", totalAmountToPay, fix_Charge);
 }
+
 int main()
 {
   int unit;
-  char choice[20];
+  char choice;
+  float total_used, fix_Chrage;
+
 repeat:
   printf("*****Enter to choice*****\n");
   printf("Press V for village area\n");
   printf("Press C for city area\n");
   printf("Press I for industril area\n");
   printf("Press A for agriculture area\n");
-  scanf("%s", &choice);
-  int n = sizeof(choice)/sizeof(choice[0]); 
-  printf("%d\n",choice[1]);
+  choice = getche();
 
-  if (choice[0] != 'V' && choice[0] != 'v' && choice[0] != 'c' && choice[0] != 'C' && choice[0] != 'i' && choice[0] != 'I' && choice[0] != 'a' && choice[0] != 'A')
+
+  if (choice != 'V' && choice != 'v' && choice != 'c' && choice != 'C' && choice != 'i' && choice != 'I' && choice != 'a' && choice != 'A')
   {
     printf("\n\n******Enter valid input******\n\n\n");
     goto repeat;
   }
 
-  printf("Enter Unit\n");
+  printf("\nEnter Unit\n");
   scanf("%d", &unit);
-  float total_used, fix_Chrage;
+  
 
-  if (choice[0] == 'V' || choice[0] == 'v')
+// For village area
+  if (choice == 'V' || choice == 'v')
   {
-    // printf("village area\n");
+    
 
     if (unit <= 24) // Count for frist 24 unit
     {
@@ -63,9 +65,11 @@ repeat:
     printBill(total_used, fix_Chrage); // Function call for print Total_bill_amount
   }
 
+
   // For city area
 
-  else if (choice[0] == 'c' || choice[0] == 'C')
+
+  else if (choice == 'c' || choice == 'C')
   {
     printf("city area\n");
     if (unit <= 24) // Count for frist 24 unit
@@ -96,41 +100,43 @@ repeat:
   // For industril area
 
 
-  else if (choice[0] == 'i' || choice[0] == 'I')
+  else if (choice == 'i' || choice == 'I')
   {
-    // printf("industril area\n");
+ 
 
-    if (unit <= 150) // Count for frist 150 unit
+    if (unit <= 150) // Count amount for frist 150 unit
     {
       total_used = unit * 9;
-      fix_Chrage = 300; // That is fix charge for using conection of power
+      fix_Chrage = 300;   // That is fix charge for using conection of power
     }
-    else if (unit <= 350) // Count for 350 - 150 unit
+    else if (unit <= 350)   // Count amount for 350 - 150 unit
     {
       total_used = 1350 + (unit - 150) * 10.5;
       fix_Chrage = 500;
     }
-    else if (unit <= 500) // Count for 500 - 350 - 150 unit
+    else if (unit <= 500)   // Count amount for 500 - 350 - 150 unit
     {
       total_used = 3450 + (unit - 350) * 11.8;
       fix_Chrage = 740;
     }
-    else // Incase unit is greatter that 500 unit than
+    else  // Incase unit is greatter that 500 unit than
     {
       total_used = 5385 + (unit - 500) * 12.9;
       fix_Chrage = 800;
     }
 
-    printBill(total_used, fix_Chrage); // Function call for print Total_bill_amount
+    printBill(total_used, fix_Chrage);  // Function call for print Total_bill_amount
 
   }
 
-  else if (choice[0] == 'A' || choice[0] == 'a')
-  {
-    // printf("agriculture area\n");
 
-    total_used = unit * 0.60;
-    fix_Chrage = 250;
+// for agriculture area
+
+  else if (choice == 'A' || choice == 'a')
+  {
+
+    total_used = unit * 0.60;   // count amount for unit
+    fix_Chrage = 250;           // fix charge for usnig power connection
 
     printBill(total_used, fix_Chrage); // Function call for print Total_bill_amount
   }
